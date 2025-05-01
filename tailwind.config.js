9df2220727f7a22a75896a0bacaa4d6d31cc2454
mainaenock <mainaenock10@gmal.com>
2025-05-01 16:31:03 +0300

    dependancies

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/**/*.{html,js,ts,jsx,tsx,vue}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
+module.exports = {
+    content: [
+        "./src/**/*.{html,js,ts,jsx,tsx,vue}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require("tailwindcss-animate"),
+    ],
+};
